b317e410af83a0f95ffe2816643a424b7b6c9071
Guy Perkal <perkal@gmail.com>
2020-06-03 11:06:21 +0300

    Updates

---
+++ precache-manifest.0d12eed48bf905bdc8be40f0afb7a30c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26c3890227afe8a177b8be37722caf1",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "a09cc316e5720a625b3c",
+    "url": "/minesweeper/static/css/main.39a37e91.chunk.css"
+  },
+  {
+    "revision": "4a432b3d304f0a54ac50",
+    "url": "/minesweeper/static/js/2.1aa5a5d9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/minesweeper/static/js/2.1aa5a5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09cc316e5720a625b3c",
+    "url": "/minesweeper/static/js/main.c9d6c647.chunk.js"
+  },
+  {
+    "revision": "75c732f2aae1e0a1c0a7",
+    "url": "/minesweeper/static/js/runtime-main.a24c26e6.js"
+  }
+]);
